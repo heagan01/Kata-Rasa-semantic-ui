13b6d463713462d4180839c61d6ffcefc0936868
Heagan <heagan866@gmail.com>
2019-10-04 20:23:58 +0700

    Commited redo on index.html to add features such as (dark mode, new UI)

--- src/index.js
+++ src/index.js
@@ -1,66 +1,260 @@
-var home = document.getElementById("home2");
+function first() {
+  var lightbtn = document.getElementById("light-button");
+  var buybody = document.getElementById("buybody");
 
-var flavours = document.getElementById("flavours2");
-
-var jokes = document.getElementById("jokes2");
-
-var comments = document.getElementById("comments2");
-
-var gallery = document.getElementById("gallery2");
-
-var feedback = document.getElementById("feedback2");
-
-function home1() {
-  home.classList.add("active");
-  flavours.classList.remove("active");
-  jokes.classList.remove("active");
-  location.href = "#home2";
-  comments.classList.remove("active");
-  gallery.classList.remove("active");
-  feedback.classList.remove("active");
+  buybody.style.display = "none";
+  lightbtn.style.display = "none";
 }
 
-function flavours1() {
-  home.classList.remove("active");
-  flavours.classList.add("active");
-  jokes.classList.remove("active");
-  comments.classList.remove("active");
-  gallery.classList.remove("active");
-  feedback.classList.remove("active");
+function swapStyleSheet(sheet) {
+  document.getElementById("pagestyle").setAttribute("href", sheet);
 }
 
-function jokes1() {
-  home.classList.remove("active");
-  flavours.classList.remove("active");
-  jokes.classList.add("active");
-  comments.classList.remove("active");
-  gallery.classList.remove("active");
-  feedback.classList.remove("active");
+function darktheme() {
+  var darkbtn = document.getElementById("dark-button");
+  var lightbtn = document.getElementById("light-button")
+  var nav = document.getElementById("navbar");
+  var buybtnnav = document.getElementById("buy-btn-nav");
+  var buybtnbody = document.getElementById("buy-btn-body");
+  var buybtnoriginal = document.getElementById("buy-original");
+  var buybtnsambalado = document.getElementById("buy-sambalado");
+
+  darkbtn.style.display = "none";
+  lightbtn.style.display = "block";
+  nav.classList.add("inverted");
+  buybtnnav.classList.add("inverted");
+  buybtnbody.classList.add("inverted");
+  buybtnoriginal.classList.add("inverted");
+  buybtnsambalado.classList.add("inverted");
+  swapStyleSheet("semantic.cyborg.css");
 }
 
-function comments1() {
-  home.classList.remove("active");
-  flavours.classList.remove("active");
-  jokes.classList.remove("active");
-  comments.classList.add("active");
-  gallery.classList.remove("active");
-  feedback.classList.remove("active");
+
+function lighttheme() {
+  var darkbtn = document.getElementById("dark-button");
+  var lightbtn = document.getElementById("light-button")
+  var nav = document.getElementById("navbar");
+  var buybtnnav = document.getElementById("buy-btn-nav");
+  var buybtnbody = document.getElementById("buy-btn-body");
+  var buybtnoriginal = document.getElementById("buy-original");
+  var buybtnsambalado = document.getElementById("buy-sambalado");
+
+  darkbtn.style.display = "block";
+  lightbtn.style.display = "none";
+  nav.classList.remove("inverted");
+  buybtnnav.classList.remove("inverted");
+  buybtnbody.classList.remove("inverted");
+  buybtnoriginal.classList.remove("inverted");
+  buybtnsambalado.classList.remove("inverted");
+  swapStyleSheet("semantic.min.css");
 }
 
-function gallery1() {
-  home.classList.remove("active");
-  flavours.classList.remove("active");
-  jokes.classList.remove("active");
-  comments.classList.remove("active");
-  gallery.classList.add("active");
-  feedback.classList.remove("active");
+function homenava() {
+  var homenav = document.getElementById("home-nav");
+  var flavoursnav = document.getElementById("flavours-nav");
+  var jokesnav = document.getElementById("jokes-nav");
+  var commentsnav = document.getElementById("comments-nav");
+  var gallerynav = document.getElementById("gallery-nav");
+  var feedbacknav = document.getElementById("feedback-nav");
+  var buybody = document.getElementById("buybody");
+  var homebody = document.getElementById("home");
+  var flavoursbody = document.getElementById("flavours");
+  var jokesbody = document.getElementById("jokes");
+  var commentsbody = document.getElementById("comments");
+  var gallerybody = document.getElementById("gallery");
+  var feedbackbody = document.getElementById("feedback");
+
+  homebody.style.display = "block";
+  flavoursbody.style.display = "block";
+  jokesbody.style.display = "block";
+  commentsbody.style.display = "block";
+  gallerybody.style.display = "block";
+  feedbackbody.style.display = "block";
+  buybody.style.display = "none";
+  homenav.classList.add("active");
+  flavoursnav.classList.remove("active");
+  jokesnav.classList.remove("active");
+  commentsnav.classList.remove("active");
+  gallerynav.classList.remove("active");
+  feedbacknav.classList.remove("active");
 }
 
-function feedback1() {
-  home.classList.remove("active");
-  flavours.classList.remove("active");
-  jokes.classList.remove("active");
-  comments.classList.remove("active");
-  gallery.classList.remove("active");
-  feedback.classList.add("active");
+function flavoursnava() {
+  var homenav = document.getElementById("home-nav");
+  var flavoursnav = document.getElementById("flavours-nav");
+  var jokesnav = document.getElementById("jokes-nav");
+  var commentsnav = document.getElementById("comments-nav");
+  var gallerynav = document.getElementById("gallery-nav");
+  var feedbacknav = document.getElementById("feedback-nav");
+  var buybody = document.getElementById("buybody");
+  var homebody = document.getElementById("home");
+  var flavoursbody = document.getElementById("flavours");
+  var jokesbody = document.getElementById("jokes");
+  var commentsbody = document.getElementById("comments");
+  var gallerybody = document.getElementById("gallery");
+  var feedbackbody = document.getElementById("feedback");
+
+  homebody.style.display = "block";
+  flavoursbody.style.display = "block";
+  jokesbody.style.display = "block";
+  commentsbody.style.display = "block";
+  gallerybody.style.display = "block";
+  feedbackbody.style.display = "block";
+  buybody.style.display = "none";
+  homenav.classList.remove("active");
+  flavoursnav.classList.add("active");
+  jokesnav.classList.remove("active");
+  commentsnav.classList.remove("active");
+  gallerynav.classList.remove("active");
+  feedbacknav.classList.remove("active");
+}
+
+function jokesnava() {
+  var homenav = document.getElementById("home-nav");
+  var flavoursnav = document.getElementById("flavours-nav");
+  var jokesnav = document.getElementById("jokes-nav");
+  var commentsnav = document.getElementById("comments-nav");
+  var gallerynav = document.getElementById("gallery-nav");
+  var feedbacknav = document.getElementById("feedback-nav");
+  var buybody = document.getElementById("buybody");
+  var homebody = document.getElementById("home");
+  var flavoursbody = document.getElementById("flavours");
+  var jokesbody = document.getElementById("jokes");
+  var commentsbody = document.getElementById("comments");
+  var gallerybody = document.getElementById("gallery");
+  var feedbackbody = document.getElementById("feedback");
+
+  homebody.style.display = "block";
+  flavoursbody.style.display = "block";
+  jokesbody.style.display = "block";
+  commentsbody.style.display = "block";
+  gallerybody.style.display = "block";
+  feedbackbody.style.display = "block";
+  buybody.style.display = "none";
+  homenav.classList.remove("active");
+  flavoursnav.classList.remove("active");
+  jokesnav.classList.add("active");
+  commentsnav.classList.remove("active");
+  gallerynav.classList.remove("active");
+  feedbacknav.classList.remove("active");
+}
+
+function commentsnava() {
+  var homenav = document.getElementById("home-nav");
+  var flavoursnav = document.getElementById("flavours-nav");
+  var jokesnav = document.getElementById("jokes-nav");
+  var commentsnav = document.getElementById("comments-nav");
+  var gallerynav = document.getElementById("gallery-nav");
+  var feedbacknav = document.getElementById("feedback-nav");
+  var buybody = document.getElementById("buybody");
+  var homebody = document.getElementById("home");
+  var flavoursbody = document.getElementById("flavours");
+  var jokesbody = document.getElementById("jokes");
+  var commentsbody = document.getElementById("comments");
+  var gallerybody = document.getElementById("gallery");
+  var feedbackbody = document.getElementById("feedback");
+
+  homebody.style.display = "block";
+  flavoursbody.style.display = "block";
+  jokesbody.style.display = "block";
+  commentsbody.style.display = "block";
+  gallerybody.style.display = "block";
+  feedbackbody.style.display = "block";
+  buybody.style.display = "none";
+  homenav.classList.remove("active");
+  flavoursnav.classList.remove("active");
+  jokesnav.classList.remove("active");
+  commentsnav.classList.add("active");
+  gallerynav.classList.remove("active");
+  feedbacknav.classList.remove("active");
+}
+
+function gallerynava() {
+  var homenav = document.getElementById("home-nav");
+  var flavoursnav = document.getElementById("flavours-nav");
+  var jokesnav = document.getElementById("jokes-nav");
+  var commentsnav = document.getElementById("comments-nav");
+  var gallerynav = document.getElementById("gallery-nav");
+  var feedbacknav = document.getElementById("feedback-nav");
+  var buybody = document.getElementById("buybody");
+  var homebody = document.getElementById("home");
+  var flavoursbody = document.getElementById("flavours");
+  var jokesbody = document.getElementById("jokes");
+  var commentsbody = document.getElementById("comments");
+  var gallerybody = document.getElementById("gallery");
+  var feedbackbody = document.getElementById("feedback");
+
+  homebody.style.display = "block";
+  flavoursbody.style.display = "block";
+  jokesbody.style.display = "block";
+  commentsbody.style.display = "block";
+  gallerybody.style.display = "block";
+  feedbackbody.style.display = "block";
+  buybody.style.display = "none";
+  homenav.classList.remove("active");
+  flavoursnav.classList.remove("active");
+  jokesnav.classList.remove("active");
+  commentsnav.classList.remove("active");
+  gallerynav.classList.add("active");
+  feedbacknav.classList.remove("active");
+}
+
+function feedbacknava() {
+  var homenav = document.getElementById("home-nav");
+  var flavoursnav = document.getElementById("flavours-nav");
+  var jokesnav = document.getElementById("jokes-nav");
+  var commentsnav = document.getElementById("comments-nav");
+  var gallerynav = document.getElementById("gallery-nav");
+  var feedbacknav = document.getElementById("feedback-nav");
+  var buybody = document.getElementById("buybody");
+  var homebody = document.getElementById("home");
+  var flavoursbody = document.getElementById("flavours");
+  var jokesbody = document.getElementById("jokes");
+  var commentsbody = document.getElementById("comments");
+  var gallerybody = document.getElementById("gallery");
+  var feedbackbody = document.getElementById("feedback");
+
+  homebody.style.display = "block";
+  flavoursbody.style.display = "block";
+  jokesbody.style.display = "block";
+  commentsbody.style.display = "block";
+  gallerybody.style.display = "block";
+  feedbackbody.style.display = "block";
+  buybody.style.display = "none";
+  homenav.classList.remove("active");
+  flavoursnav.classList.remove("active");
+  jokesnav.classList.remove("active");
+  commentsnav.classList.remove("active");
+  gallerynav.classList.remove("active");
+  feedbacknav.classList.add("active");
+}
+
+function buy() {
+  var homenav = document.getElementById("home-nav");
+  var flavoursnav = document.getElementById("flavours-nav");
+  var jokesnav = document.getElementById("jokes-nav");
+  var commentsnav = document.getElementById("comments-nav");
+  var gallerynav = document.getElementById("gallery-nav");
+  var feedbacknav = document.getElementById("feedback-nav");
+  var buybody = document.getElementById("buybody");
+  var homebody = document.getElementById("home");
+  var flavoursbody = document.getElementById("flavours");
+  var jokesbody = document.getElementById("jokes");
+  var commentsbody = document.getElementById("comments");
+  var gallerybody = document.getElementById("gallery");
+  var feedbackbody = document.getElementById("feedback");
+
+  homebody.style.display = "none";
+  flavoursbody.style.display = "none";
+  jokesbody.style.display = "none";
+  commentsbody.style.display = "none";
+  gallerybody.style.display = "none";
+  feedbackbody.style.display = "none";
+  homenav.classList.remove("active");
+  flavoursnav.classList.remove("active");
+  jokesnav.classList.remove("active");
+  commentsnav.classList.remove("active");
+  gallerynav.classList.remove("active");
+  feedbacknav.classList.remove("active");
+  buybody.style.display = "block";
 }
